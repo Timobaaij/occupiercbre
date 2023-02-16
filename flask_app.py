ec67607350912997cdc32b3cb58208d11ee6e160
Timobaaij <39728252+Timobaaij@users.noreply.github.com>
2023-02-16 15:15:23 +0100

    Add files via upload

---
+++ flask_app.py
@@ -0,0 +1,107 @@
+from flask import Flask, render_template, request, send_file
+import os
+from pptx import Presentation
+import pandas as pd
+from PIL import Image
+import io
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['STATIC_FOLDER'] = 'static'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/download_template')
+def download_template():
+    return send_file(os.path.join(app.config['STATIC_FOLDER'], 'template.xlsx'), as_attachment=True)
+
+@app.route('/upload_files', methods=['POST'])
+def upload_files():
+    # Handle uploaded data.xlsx file
+    data_file = request.files['data_file']
+    data_path = os.path.join(app.config['UPLOAD_FOLDER'], 'data.xlsx')
+    data_file.save(data_path)
+
+    # Handle uploaded picture files
+    picture_paths = []
+    for picture_file in request.files.getlist('picture_files'):
+        picture_path = os.path.join(app.config['UPLOAD_FOLDER'], picture_file.filename)
+        picture_file.save(picture_path)
+        picture_paths.append(picture_path)
+
+    # Load data from Excel file
+    data = pd.read_excel(data_path)
+
+    # Load PowerPoint template and select slide layout
+    prs = Presentation(os.path.join(app.config['STATIC_FOLDER'], 'template.pptx'))
+    layout = prs.slide_layouts[0]
+
+    # Loop over rows in Excel file and create new slide for each row
+    for index, row in data.iterrows():
+        slide = prs.slides.add_slide(layout)
+
+        # Populate text placeholders on slide with values from Excel row
+        slide.shapes.placeholders[14].text = str(row['Titel'])
+        slide.shapes.placeholders[10].text = str(row['Site'])
+        slide.shapes.placeholders[11].text = str(row['Building'])
+        slide.shapes.placeholders[12].text = str(row['Office'])
+        slide.shapes.placeholders[13].text = str(row['Mezzanine'])
+        slide.shapes.placeholders[17].text = str(row['Parking'])
+        slide.shapes.placeholders[18].text = str(row['Environment category'])
+        slide.shapes.placeholders[19].text = str(row['Maximum building height'])
+        slide.shapes.placeholders[20].text = str(row['Clear height'])
+        slide.shapes.placeholders[21].text = str(row['Floor load'])
+        slide.shapes.placeholders[22].text = str(row['Floor flatness'])
+        slide.shapes.placeholders[23].text = str(row['Loading docks'])
+        slide.shapes.placeholders[24].text = str(row['Overhead doors'])
+        slide.shapes.placeholders[25].text = str(row['Sprinkler'])
+
+        # Calculate the index of the first picture on this slide
+        first_picture_index = 15
+
+        # Construct image paths based on image filenames from Excel file
+        image1_filename = str(row['Image 1'])
+        image1_path = os.path.join(app.config['UPLOAD_FOLDER'], image1_filename + '.jpg')
+        image2_filename = str(row['Image 2'])
+        image2_path = os.path.join(app.config['UPLOAD_FOLDER'], image2_filename + '.jpg')
+
+        # Check if image paths are valid
+        if not os.path.isfile(image1_path):
+            print(f'Error: Could not find {image1_path}')
+        else:
+            with open(image1_path, 'rb') as f:
+                img_bytes = f.read()
+
+            slide.shapes.placeholders[first_picture_index].insert_picture(io.BytesIO(img_bytes))
+
+        if not os.path.isfile(image1_path):
+            print(f'Error: Could not find {image1_path}')
+        else:
+            with open(image1_path, 'rb') as f:
+                img_bytes = f.read()
+
+            slide.shapes.placeholders[first_picture_index].insert_picture(io.BytesIO(img_bytes))
+
+        if not os.path.isfile(image2_path):
+            print(f'Error: Could not find {image2_path}')
+        else:
+            with open(image2_path, 'rb') as f:
+                img_bytes = f.read()
+
+            slide.shapes.placeholders[first_picture_index+1].insert_picture(io.BytesIO(img_bytes))
+
+    # Save populated PowerPoint file
+    prs.save(os.path.join(app.config['UPLOAD_FOLDER'], 'mypopulated.pptx'))
+    
+    # Delete temporary data and picture files
+    os.remove(data_path)
+    for picture_path in picture_paths:
+        os.remove(picture_path)
+    
+    # Return populated PowerPoint file for download
+    return send_file(os.path.join(app.config['UPLOAD_FOLDER'], 'mypopulated.pptx'), as_attachment=True)
+
+if name == 'main':
+app.run(debug=True)
